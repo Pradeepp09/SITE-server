41c43a2d8c8aa8ba4c19630f260ac9fe534d4914
Pradeepp09 <115462043+Pradeepp09@users.noreply.github.com>
2025-04-17 13:31:39 +0530

    Create cloudinary.js
    ADDED CLOUDINARY.JS

---
+++ cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dv9ymlooy',
+  api_key: '368688198613512',
+  api_secret: 'pim7oXRpHr0bNaynIEK5aHSlvh8',
+});
+
+module.exports = cloudinary;
